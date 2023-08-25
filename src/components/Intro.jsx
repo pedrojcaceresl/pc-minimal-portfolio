5aeff51477cb0a166458336ddb8c2fe32f842fb0
pedro <pedro.jcaceresl@gmail.com>
2023-08-25 10:07:25 -0400

    description updated

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -5,27 +5,34 @@ const Intro = () => {
     return (
 		<div className="flex items-center justify-center flex-col text-center pt-20 pb-6">
 			<h1 className="text-4xl md:text-7xl mb-1 md:mb-3 font-bold">
-				Hi! I'm Pedro
+				Hello, I'm Pedro!
 			</h1>
 			<p className="text-base md:text-xl mb-3 font-medium">
-				Full Stack Developer
+				Web Developer
 			</p>
 			<p className="text-sm max-w-xl mb-6 font-bold">
-				Hi, I’m Pedro Juan Caceres, a full stack developer with one year
-				of experience in creating responsive and user-friendly websites.
-				I specialize in HTML, Tailwind, JavaScript, TypeScript, Node
-				and Angular development.
-				{/* I have worked on projects for various clients... */}
-				<br/><br/>
-				I’m passionate about developement and always strive to learn new
-				technologies and best practices. I also have a{" "}
-				<a className='text-blue-400' href="https://medium.com/@pedro.jcaceresl">blog on medium </a>
-				and a{" "}
+				Greetings! I'm Pedro Caceres, a web developer with
+				nearly two years of experience crafting responsive and
+				user-friendly websites. My skill set includes HTML, Tailwind
+				CSS, JavaScript, TypeScript, Node.js, React, and Angular
+				development.
+				<br />
+				<br />
+				My passion for development drives me to constantly explore new
+				technologies and adopt best practices. You can also find me
+				sharing my insights through my{" "}
+				<a
+					className="text-blue-400"
+					href="https://medium.com/@pedro.jcaceresl"
+				>
+					Medium blog
+				</a>{" "}
+				and hosting a{" "}
 				<a
 					className="text-blue-400"
 					href="https://open.spotify.com/show/2kNjETgrswYSu4Eu6sLRDX"
 				>
-					podcast in Spanish on Spotify
+					Spanish-language podcast on Spotify
 				</a>
 				.
 			</p>
